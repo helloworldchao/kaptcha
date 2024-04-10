741cde9e927e9a370355a9254fa4560616ae6595
yuchao <helloworldchao@outlook.com>
2024-04-10 09:50:38 +0800

    feat: add create base64 image

--- src/main/java/tech/helloworldchao/kaptcha/impl/DefaultKaptcha.java
+++ src/main/java/tech/helloworldchao/kaptcha/impl/DefaultKaptcha.java
@@ -1,11 +1,5 @@
 package tech.helloworldchao.kaptcha.impl;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.geom.Line2D;
-import java.awt.image.BufferedImage;
-
 import tech.helloworldchao.kaptcha.BackgroundProducer;
 import tech.helloworldchao.kaptcha.GimpyEngine;
 import tech.helloworldchao.kaptcha.Producer;
@@ -13,72 +7,91 @@ import tech.helloworldchao.kaptcha.text.TextProducer;
 import tech.helloworldchao.kaptcha.text.WordRenderer;
 import tech.helloworldchao.kaptcha.util.Configurable;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
 /**
  * Default {@link Producer} implementation which draws a captcha image using
  * {@link WordRenderer}, {@link GimpyEngine}, {@link BackgroundProducer}.
  * Text creation uses {@link TextProducer}.
  */
-public class DefaultKaptcha extends Configurable implements Producer
-{
-	private int width = 200;
+public class DefaultKaptcha extends Configurable implements Producer {
+    private int width = 200;
 
-	private int height = 50;
+    private int height = 50;
 
-	/**
-	 * Create an image which will have written a distorted text.
-	 * 
-	 * @param text
-	 *            the distorted characters
-	 * @return image with the text
-	 */
-	public BufferedImage createImage(String text)
-	{
-		WordRenderer wordRenderer = getConfig().getWordRendererImpl();
-		GimpyEngine gimpyEngine = getConfig().getObscurificatorImpl();
-		BackgroundProducer backgroundProducer = getConfig().getBackgroundImpl();
-		boolean isBorderDrawn = getConfig().isBorderDrawn();
-		this.width = getConfig().getWidth();
-		this.height = getConfig().getHeight();
+    /**
+     * Create an image which will have written a distorted text.
+     *
+     * @param text the distorted characters
+     * @return image with the text
+     */
+    public BufferedImage createImage(String text) {
+        WordRenderer wordRenderer = getConfig().getWordRendererImpl();
+        GimpyEngine gimpyEngine = getConfig().getObscurificatorImpl();
+        BackgroundProducer backgroundProducer = getConfig().getBackgroundImpl();
+        boolean isBorderDrawn = getConfig().isBorderDrawn();
+        this.width = getConfig().getWidth();
+        this.height = getConfig().getHeight();
 
-		BufferedImage bi = wordRenderer.renderWord(text, width, height);
-		bi = gimpyEngine.getDistortedImage(bi);
-		bi = backgroundProducer.addBackground(bi);
-		Graphics2D graphics = bi.createGraphics();
-		if (isBorderDrawn)
-		{
-			drawBox(graphics);
-		}
-		return bi;
-	}
+        BufferedImage bi = wordRenderer.renderWord(text, width, height);
+        bi = gimpyEngine.getDistortedImage(bi);
+        bi = backgroundProducer.addBackground(bi);
+        Graphics2D graphics = bi.createGraphics();
+        if (isBorderDrawn) {
+            drawBox(graphics);
+        }
+        return bi;
+    }
 
-	private void drawBox(Graphics2D graphics)
-	{
-		Color borderColor = getConfig().getBorderColor();
-		int borderThickness = getConfig().getBorderThickness();
+    private void drawBox(Graphics2D graphics) {
+        Color borderColor = getConfig().getBorderColor();
+        int borderThickness = getConfig().getBorderThickness();
 
-		graphics.setColor(borderColor);
+        graphics.setColor(borderColor);
 
-		if (borderThickness != 1)
-		{
-			BasicStroke stroke = new BasicStroke((float) borderThickness);
-			graphics.setStroke(stroke);
-		}
+        if (borderThickness != 1) {
+            BasicStroke stroke = new BasicStroke((float) borderThickness);
+            graphics.setStroke(stroke);
+        }
 
-		Line2D line1 = new Line2D.Double(0, 0, 0, width);
-		graphics.draw(line1);
-		Line2D line2 = new Line2D.Double(0, 0, width, 0);
-		graphics.draw(line2);
-		line2 = new Line2D.Double(0, height - 1, width, height - 1);
-		graphics.draw(line2);
-		line2 = new Line2D.Double(width - 1, height - 1, width - 1, 0);
-		graphics.draw(line2);
-	}
+        Line2D line1 = new Line2D.Double(0, 0, 0, width);
+        graphics.draw(line1);
+        Line2D line2 = new Line2D.Double(0, 0, width, 0);
+        graphics.draw(line2);
+        line2 = new Line2D.Double(0, height - 1, width, height - 1);
+        graphics.draw(line2);
+        line2 = new Line2D.Double(width - 1, height - 1, width - 1, 0);
+        graphics.draw(line2);
+    }
 
-	/**
-	 * @return the text to be drawn
-	 */
-	public String createText()
-	{
-		return getConfig().getTextProducerImpl().getText();
-	}
+    /**
+     * Create a Base64 image which will have written a distorted text.
+     *
+     * @param text the distorted characters
+     * @return Base64 image with the text
+     */
+    public String createBase64Image(String text) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(createImage(text), "jpg", stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        byte[] bytes = Base64.getEncoder().encode(stream.toByteArray());
+        String base64 = new String(bytes);
+        return "data:image/jpeg;base64," + base64;
+    }
+
+    /**
+     * @return the text to be drawn
+     */
+    public String createText() {
+        return getConfig().getTextProducerImpl().getText();
+    }
 }
